e0d1339321ac534a67e5ebf8b61569fa1ef1e2d2
kanetah <2205485120@qq.com>
2017-11-14 18:04:16 +0800

    debug: message is too short

--- hotNewsPage/src/main/mysql/crawler_info.sql
+++ hotNewsPage/src/main/mysql/crawler_info.sql
@@ -7,5 +7,5 @@ CREATE TABLE crawler_info
   final_date DATE        NULL,
   count      INT         NULL,
   state      VARCHAR(45) NOT NULL,
-  message    VARCHAR(45) NOT NULL
+  message    VARCHAR(1000) NOT NULL
 );
