0c55fe5d09265ed8c0098ad5eb5f8fbc8b718867
kanetah <2205485120@qq.com>
2017-11-25 20:14:25 +0800

    Create CommentDTO.java
    exception when rename dto package

---
+++ hotNewsPage/src/main/java/top/kanetah/hotNewsCrawler/dto/CommentDTO.java
@@ -0,0 +1,57 @@
+package top.kanetah.hotNewsCrawler.dto;
+
+public class CommentDTO {
+    private int id;
+    private String userName;
+    private String newsTitle;
+    private String content;
+    private String time;
+
+    public CommentDTO(int id, String userName, String newsTitle, String content, String time) {
+        this.id = id;
+        this.userName = userName;
+        this.newsTitle = newsTitle;
+        this.content = content;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNewsTitle() {
+        return newsTitle;
+    }
+
+    public void setNewsTitle(String newsTitle) {
+        this.newsTitle = newsTitle;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
